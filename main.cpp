09c29cfa024cdd575496af82902bfb6e50d9bea9
LucaBorzone13 <92693790+LucaBorzone13@users.noreply.github.com>
2021-12-01 19:53:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 
-int main(){
-  return 0;
+
+
+char conversione(char x){
+
+    char ris;
+
+    if( ((x>='a') && (x<='z')) || ((x>='A') && (x<='Z')) ) {
+
+            std::cout << "vero" << std::endl;
+
+    }else if((x!='a') && (x!='z')) {
+
+        std::cout << "errore" << std::endl;
+    }
+
+    if(x>='A' && x <= 'Z'){
+
+        ris = x+32;
+
+    }else if (x>='a' && x <= 'z') {
+
+        ris = x-32;
+    }
+    return  ris;
 }
+
+int main() {
+
+
+    char x;
+
+
+    std::cin >> x;
+
+    char z =conversione(x);
+
+    std::cout << z << std::endl;
+
+
+    return 0;
+}
+
+
+
+
